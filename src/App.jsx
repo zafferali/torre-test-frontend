a618c35296c39ca3c459cfc59a917062f5a8db75
Zaffer Ali <87249334+zafferali@users.noreply.github.com>
2023-06-01 20:20:22 +0530

    Add tailwind and structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from "./pages/Home"
+
 function App() {
 
   return (
-    <>
-      Hello, World!
-    </>
+   <div>
+    <Home />
+   </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <div className="bg-slate-900">Home</div>
+  )
+}
+
+export default Home
